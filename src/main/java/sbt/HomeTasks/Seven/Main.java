bbe3fb0d3b8f755f773b465544a403f98af874f9
SBTJavastudent <tyuhgce@gmail.com>
2016-09-01 20:15:22 +0700

    7.1

--- src/main/java/sbt/HomeTasks/Seven/Main.java
+++ src/main/java/sbt/HomeTasks/Seven/Main.java
@@ -12,13 +12,10 @@ class Main {
         Calculator calcProxy = (Calculator) Proxy.newProxyInstance(MyCalculator.class.getClassLoader(),
                 MyCalculator.class.getInterfaces(),
                 new CachedCalculator(calc));
-
         System.out.println(String.format("1 + 2 = " + calcProxy.perform(new Operation(1, 2, "PLUS"))));
         System.out.println(String.format("1 + 2 = " + calcProxy.perform(new Operation(1, 2, "PLUS"))));
         System.out.println(String.format("1 - 1 = " + calcProxy.perform(new Operation(1, 1, "MINUS"))));
         System.out.println(String.format("4 * 6 = " + calcProxy.perform(new Operation(4, 6, "MULTIPLY"))));
         System.out.println(String.format("1 - 1 = " + calcProxy.perform(new Operation(1, 1, "MINUS"))));
-
-
     }
 }
